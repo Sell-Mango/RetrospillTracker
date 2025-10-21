4e0c8039d8ca9c65c608f0af81eb16485632a9b2
Lubbsaur <berghjunior@gmail.com>
2025-10-21 12:10:01 +0200

    Tester med API-call

--- src/app/pages/ProfilePage.tsx
+++ src/app/pages/ProfilePage.tsx
@@ -1,4 +1,31 @@
+import { useEffect, useState } from "react"
+
 export default function ProfilePage(){
+  const [data, setData] = useState();
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const GET_URL = "https://api.igdb.com/v4/games";
+  
+  useEffect(() => {
+    async function fetch
+    
+    setLoading(true)
+    fetch(GET_URL, {headers: {
+      "Accept": "application/json",
+      "Access-Control-Allow-Origin": "*"
+    }})
+    .then(res => {
+      return (res.ok) ? res.json() : new Error("Mistake!")
+    })
+    .then(data => {
+      setData(data)
+      setLoading(false)
+    })
+    .catch(error => {
+      setError(error)
+    })
+  }, [])
+  
   return (
     <h1>Profile page</h1>
   )

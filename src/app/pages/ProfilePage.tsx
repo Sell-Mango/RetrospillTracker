7375a4d65cfbc6555b6c4119f70f680c2876da83
Lubbsaur <berghjunior@gmail.com>
2025-10-21 14:13:15 +0200

    Tester API

--- src/app/pages/ProfilePage.tsx
+++ src/app/pages/ProfilePage.tsx
@@ -1,29 +1,27 @@
 import { useEffect, useState } from "react"
 
 export default function ProfilePage(){
-  const [data, setData] = useState();
+  const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const GET_URL = "https://api.igdb.com/v4/games";
   
   useEffect(() => {
-    async function fetch
-    
-    setLoading(true)
-    fetch(GET_URL, {headers: {
-      "Accept": "application/json",
-      "Access-Control-Allow-Origin": "*"
-    }})
-    .then(res => {
-      return (res.ok) ? res.json() : new Error("Mistake!")
-    })
-    .then(data => {
-      setData(data)
-      setLoading(false)
-    })
-    .catch(error => {
-      setError(error)
-    })
+    async function fetchData() {
+      try {
+        const response = await fetch(GET_URL, {
+          method: "POST",
+          headers: {
+            "Client-ID": "",
+            "Authorization": "",
+            "Accept": "application/json"
+          }
+        });
+        if (!response.ok) throw new Error("Error")
+        const result = await response.json()
+        setData(result)
+      }
+    }
   }, [])
   
   return (

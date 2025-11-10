34b408cca4abcb8886b871c716590d5cd40bc904
Lubbsaur <berghjunior@gmail.com>
2025-11-10 21:19:20 +0100

    Lagt til user-objekt

--- src/app/pages/SignUp.tsx
+++ src/app/pages/SignUp.tsx
@@ -1,14 +1,32 @@
 "use client";
 
-import { useState } from "react";
+import { use, useState } from "react";
 
 export default function SignUp(){
   const [username, setUsername] = useState<string>("");
   const [password, setPassword] = useState<string>("");
+  const [firstname, setFirstname] = useState<string>("");
+  const [lastname, setLastname] = useState<string>("")
+  const [user, setUser] = useState({
+    firstName: "",
+    lastName: "",
+    userName: "",
+    passWord: ""
+  })
 
-  function handleLogin(e: any) {
+  function handleSignup(e: any) {
     e.preventDefault();
+    setUser(
+      {
+      firstName: firstname,
+      lastName: lastname,
+      userName: username,
+      passWord: password
+    })
   }
+
+  console.log(user)
+
   return (
     <>
     <h1 className="text-3xl font-bold text-white text-center drop-shadown-sm">Sign up</h1>
@@ -16,14 +34,14 @@ export default function SignUp(){
       <div className="w-1/2 bg-iceblue rounded-sm border-secondary p-20">
         <div className="flex flex-col gap-2">
             <h2 className="text-primary font-bold text-lg">Signup</h2>
-            <form className="flex flex-col gap-1" onSubmit={handleLogin}>
+            <form className="flex flex-col gap-1" onSubmit={handleSignup}>
                 <label>
                     First name:
-                    <input className="border bg-white w-full p-1 mb-1" type="text" placeholder="Ola..." value={username} onChange={e => setUsername(e.target.value)} />
+                    <input className="border bg-white w-full p-1 mb-1" type="text" placeholder="Ola..." value={firstname} onChange={e => setFirstname(e.target.value)} />
                 </label>
                 <label>
                     Last name:
-                    <input className="border bg-white w-full p-1 mb-1" type="text" placeholder="Nordmann..." value={username} onChange={e => setUsername(e.target.value)} />
+                    <input className="border bg-white w-full p-1 mb-1" type="text" placeholder="Nordmann..." value={lastname} onChange={e => setLastname(e.target.value)} />
                 </label>
                 <label>
                     Username:
@@ -33,9 +51,8 @@ export default function SignUp(){
                     Password:
                     <input className="bg-white border w-full p-1" type="password" placeholder="**********" value={password} onChange={e => setPassword(e.target.value)} />
                 </label>
-                <button className="border text-glow-orange mx-30 py-2 px-1 bg-primary" type="submit">Login</button>
+                <button className="border text-glow-orange mx-30 mt-15 py-2 px-1 bg-primary" type="submit">Sign up</button>
             </form>
-            <button className="text-glow-orange border px-2 mx-auto bg-white" onClick={handleLogin}>X</button>
         </div>
       </div>
     </section>

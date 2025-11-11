3a25868138d7b9fa35cc7799afafcbb79ad4e27c
Lubbsaur <berghjunior@gmail.com>
2025-11-11 16:09:50 +0100

    Skiftet farge på Login

--- src/app/pages/Login.tsx
+++ src/app/pages/Login.tsx
@@ -12,17 +12,17 @@ export default function Login(props: any){
   }
 
   return (
-    <div className="w-1/3 h-1/2 bg-iceblue fixed top-1/2 left-1/2 p-2 m-auto -translate-1/2 rounded-sm border-secondary">
+    <div className="w-1/4 h-1/2 bg-primary-light fixed top-1/2 left-1/2 p-2 m-auto -translate-1/2 rounded-sm border-4 border-iceblue">
         <div className="flex flex-col gap-2 p-25">
-            <h2 className="text-primary font-bold text-4xl text-shadow-2xs mb-3">Login</h2>
+            <h2 className="text-white font-bold text-4xl text-shadow-2xs mb-3">Login</h2>
             <form className="flex flex-col gap-1" onSubmit={handleLogin}>
-                <label className="text-2xl">
+                <label className="text-2xl text-white">
                     Username:
-                    <input className="border bg-white w-full p-1 mb-1" type="text" placeholder="Ola Nordmann..." value={username} onChange={e => setUsername(e.target.value)} />
+                    <input className="border text-black bg-white w-full p-1 mb-1" type="text" placeholder="Ola Nordmann..." value={username} onChange={e => setUsername(e.target.value)} />
                 </label>
-                <label className="text-2xl">
+                <label className="text-2xl text-white">
                     Password:
-                    <input className="bg-white border w-full p-1" type="password" placeholder="**********" value={password} onChange={e => setPassword(e.target.value)} />
+                    <input className="bg-white border w-full p-1 text-black" type="password" placeholder="**********" value={password} onChange={e => setPassword(e.target.value)} />
                 </label>
                 <button className="border text-glow-orange py-2 px-1 bg-primary my-3 text-2xl" type="submit">Login</button>
             </form>

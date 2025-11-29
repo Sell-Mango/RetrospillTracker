7a386ba1a6ee42ccd92473e2fa14b1c0933c2542
Marius <mariuror@hiof.no>
2025-11-29 19:30:54 +0100

    Added: Route for getting specific game

--- src/app/shared/config/IGDBQueries.ts
+++ src/app/shared/config/IGDBQueries.ts
@@ -3,7 +3,8 @@ const QUERY = {
     ALL_GAMES: 'fields name, cover.url, rating, rating_count, first_release_date; limit 25;',
     SEARCH_GAMES: (search:string):string => {return `fields name, cover.url, rating, rating_count, first_release_date;
     search "${search}";
-    limit 25;`}
+    limit 25;`},
+    SEARCH_GAME: (gameId:string):string => {return `fields name; where id = ${gameId}; limit 1;`;},
 }
 
 export {QUERY}
